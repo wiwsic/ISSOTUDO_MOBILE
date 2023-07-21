637e7d82fb21adfab3d7eccca0523ae8cc481cbe
WiW <william.v.dacosta@gmail.com>
2023-07-21 01:13:32 -0300

    criado novos viewports

--- rascunho.js
+++ rascunho.js
@@ -693,6 +693,452 @@ function S04_draw(){
 	}
 }
 
+//s03 bkp
+
+function build_S03(){
+	SKT = { draw: PH_draw, mouseMoved: PH_mouseMoved, mousePressed: PH_mousePressed, 
+			mouseDragged: PH_mouseDragged, mouseReleased: PH_mouseReleased,
+			end: PH_end };
+
+	//SKT.bgx = VIEWPORT.x + 0.6 * VIEWPORT.w;
+	SKT.bgx = width * .1 - (60);
+
+	SKT.Scl = VIEWPORT.h / 1699.0;
+	SKT.Sclx = VIEWPORT.w / 700.0;
+	//SKT.Scl = VIEWPORT.h / 1199.0;
+
+	SKT.dst = Array(8);
+	SKT.dst[0] = { x: 280, y: 981, w: 228,  h: 193 }; //foot.png
+	SKT.dst[1] = { x: 291, y: 108, w: 202,  h: 904 }; //left branch.png 
+	SKT.dst[2] = { x: 321, y: 639, w: 322,  h: 344 }; //right branch.png
+	SKT.dst[3] = { x: 188, y: 522, w: 126,  h: 204 }; //left leaf.png
+	SKT.dst[4] = { x: 232, y: 247, w: 170,  h: 80  }; //top leaf.png
+	SKT.dst[5] = { x: 350, y: 701, w: 227,  h: 167 }; //file folder.png 
+	SKT.dst[6] = { x: 349, y: 367, w: 217,  h: 319 }; //flower.png      
+	SKT.dst[7] = { x: 636, y: 540, w: 242,  h: 253 }; //right leaf.png  
+
+	SKT.V = Array(8);
+	SKT.V[0]= createVector( 322.102, 1008.18 ); //foot.png:         foot
+	SKT.V[1]= createVector( 307.607, 705.403 ); //left branch.png:  left leaf root
+	SKT.V[2]= createVector( 347.870, 850.349 ); //right branch.png: file root
+	SKT.V[3]= createVector( 217.419, 560.457 ); //left leaf.png:    left leaf tip  
+	SKT.V[4]= createVector( 265.734, 273.787 ); //top leaf.png:     top leaf tip
+	SKT.V[5]= createVector( 452.553, 832.634 ); //file folder.png:  file tip
+	SKT.V[6]= createVector( 457.385, 484.764 ); //flower.png:       flower tip
+	SKT.V[7]= createVector( 752.108, 665.141 ); //right leaf.png:   right leaf tip
+
+
+	var heightAdjustment = 220.5; // Valor de ajuste para diminuir a altura dos elementos
+
+	// Ajuste das coordenadas y dos vetores
+	for (var i = 0; i < 8; i++) {
+	  SKT.dst[i].y += heightAdjustment;
+	  SKT.V[i].y += heightAdjustment;
+	}	
+
+	SKT.RV = Array(8);
+	SKT.RV[0]= null;//foot
+	SKT.RV[1]= SKT.V[0].copy();//left branch
+	SKT.RV[2]= SKT.V[0].copy();//right branch
+	SKT.RV[3]= SKT.V[1].copy();//left leaf
+	SKT.RV[4]= createVector( 401.017, 318.881 ); // top leaf root //top leaf
+	SKT.RV[4].y += heightAdjustment
+	SKT.RV[5]= SKT.V[2].copy();//file folder
+	SKT.RV[6]= createVector( 426.785, 689.298 ); // flower root //flower
+	SKT.RV[6].y += heightAdjustment
+	SKT.RV[7]= createVector( 708.624, 557.236 ); // right leaf root//right leaf
+	SKT.RV[7].y += heightAdjustment
+
+	SKT.O = Array(8);
+	SKT.da = Array(8);
+	SKT.td = Array(8);
+	for (var i = 0; i < 8; i++) {
+		if( SKT.RV[i] == null ){
+			SKT.O[i] = createVector( SKT.V[i].x - SKT.dst[i].x, SKT.V[i].y - SKT.dst[i].y ).mult(SKT.Scl);
+		}
+		else{
+			SKT.O[i] = createVector( SKT.RV[i].x - SKT.dst[i].x, SKT.RV[i].y - SKT.dst[i].y ).mult(SKT.Scl);
+			SKT.da[i] = -atan2( SKT.V[i].y - SKT.RV[i].y, SKT.V[i].x - SKT.RV[i].x);
+			SKT.RV[i].mult(SKT.Scl);
+		}
+		SKT.td[i] = { w: SKT.dst[i].w * SKT.Scl, h: SKT.dst[i].h * SKT.Scl };
+		SKT.V[i].mult(SKT.Scl);
+		SKT.V[i].x += SKT.bgx;
+		if( SKT.RV[i] != null ) SKT.RV[i].x += SKT.bgx;
+	}
+
+	SKT.bo = Array(3);
+	SKT.bo[0] = p5.Vector.sub( SKT.RV[4], SKT.V[1] );
+	SKT.bo[0].rotate( -atan2( SKT.V[1].y - SKT.V[0].y, SKT.V[1].x - SKT.V[0].x) );
+	SKT.bo[1] = p5.Vector.sub( SKT.RV[6], SKT.V[2] );
+	SKT.bo[1].rotate( -atan2( SKT.V[2].y - SKT.V[0].y, SKT.V[2].x - SKT.V[0].x) );
+	SKT.bo[2] = p5.Vector.sub( SKT.RV[7], SKT.V[2] );
+	SKT.bo[2].rotate( -atan2( SKT.V[2].y - SKT.V[0].y, SKT.V[2].x - SKT.V[0].x) );
+
+	SKT.l = Array(7);
+	SKT.l[0] = p5.Vector.dist(  SKT.V[0], SKT.V[1] );
+	SKT.l[1] = p5.Vector.dist(  SKT.V[0], SKT.V[2] );
+	SKT.l[2] = p5.Vector.dist( SKT.RV[3], SKT.V[3] );
+	SKT.l[3] = p5.Vector.dist( SKT.RV[4], SKT.V[4] );
+	SKT.l[4] = p5.Vector.dist( SKT.RV[5], SKT.V[5] );
+	SKT.l[5] = p5.Vector.dist( SKT.RV[6], SKT.V[6] );
+	SKT.l[6] = p5.Vector.dist( SKT.RV[7], SKT.V[7] );
+
+	SKT.normals = Array(8);
+	for( var i = 1; i < 8; ++i ){
+		SKT.normals[i] = p5.Vector.sub( SKT.V[i], SKT.RV[i] ).mult(0.01);
+	}
+
+	SKT.wind = createVector(0,0);
+
+	SKT.img = Array(8);
+	SKT.img[0] = loadImage('data03/foot.png', build_S03_step1, failed );
+}
+function build_S03_step1(){
+	SKT.img[1] = loadImage('data03/left branch.png', build_S03_step2, failed );
+}
+function build_S03_step2(){
+	SKT.img[2] = loadImage('data03/right branch.png', build_S03_step3, failed );
+}
+function build_S03_step3(){
+	SKT.img[3] = loadImage('data03/left leaf.png', build_S03_step4, failed );
+}
+function build_S03_step4(){
+	SKT.img[4] = loadImage('data03/top leaf.png', build_S03_step5, failed );
+}
+function build_S03_step5(){
+	SKT.img[5] = loadImage('data03/file folder.png', build_S03_step6, failed );
+}
+function build_S03_step6(){
+	SKT.img[6] = loadImage('data03/flower.png', build_S03_step7, failed );
+}
+function build_S03_step7(){
+	SKT.img[7] = loadImage('data03/right leaf.png', build_S03_step8, failed );
+}
+function build_S03_step8(){
+	SKT.sound_pasta = loadSound( 'data03/01-arquivo-planta-1.wav', build_S03_step9, failed );
+	SKT.sound_pasta.playMode('sustain');
+}
+function build_S03_step9(){
+	SKT.sound_flor  = loadSound( 'data03/02-flor-do-algodao15.wav', build_S03_step10, failed );
+	SKT.sound_flor.playMode('sustain');
+}
+function build_S03_step10(){
+	SKT.sound_folhas = loadSound('data02/11.wav', build_S03_step11, failed );
+	SKT.sound_folhas.playMode('sustain');
+}
+function build_S03_step11(){
+	SKT.draw = S03_draw;
+	loop();
+}
+
+let prevTouchX = 0;
+let prevTouchY = 0;
+
+function S03_draw(){
+
+	let x = mouseX;
+	let y = mouseY;
+  
+	if (touches.length > 0) {
+		let touch = touches[0];
+		x = touch.x;
+		y = touch.y;
+	
+		let touchVelX = x - prevTouchX;
+		let touchVelY = y - prevTouchY;
+		let touchVel = createVector(touchVelX, touchVelY);
+		SKT.wind = touchVel.mult(0.08);
+	
+		prevTouchX = x;
+		prevTouchY = y;
+	  } else {
+		SKT.wind = createVector(0, 0);
+	  }
+	
+	let M = createVector(x, y);
+
+	if( p5.Vector.dist( M, SKT.V[5] ) < 140 * SKT.Scl ){
+		if( !(SKT.sound_pasta.isPlaying()) ){
+			SKT.sound_pasta.loop();
+		}
+	}
+	else{
+		SKT.sound_pasta.pause();
+	}
+	if( p5.Vector.dist( M, SKT.V[6] ) < 200 * SKT.Scl ){
+		if( !(SKT.sound_flor.isPlaying()) ){
+			SKT.sound_flor.loop();
+		}
+	}
+	else{
+		SKT.sound_flor.pause();
+	}
+
+	if( p5.Vector.dist( M, SKT.V[3] ) < 140 * SKT.Scl ||
+		p5.Vector.dist( M, SKT.V[4] ) < 140 * SKT.Scl ||
+		p5.Vector.dist( M, SKT.V[7] ) < 230 * SKT.Scl ){
+		if( !(SKT.sound_folhas.isPlaying()) ){
+			SKT.sound_folhas.loop();
+		}
+	}
+	else{
+		SKT.sound_folhas.pause();
+	}
+
+	clear();
+
+	//noFill();
+	//stroke(255);
+	//ellipse( SKT.V[5].x, SKT.V[5].y, 80, 80 );
+	//ellipse( SKT.V[6].x, SKT.V[6].y, 80, 80 );
+
+	fill(255);
+	noStroke();
+	
+
+	//SKT.wind = p5.Vector.lerp( SKT.wind, createVector( movedX, movedY ).mult(0.1), 0.1 );
+	//SKT.wind.x *= 0.99;
+	//SKT.wind.y *= 0.3;
+
+
+	for( var i = 1; i < 8; ++i ){
+		SKT.V[i].add( SKT.wind );
+		SKT.V[i].add( SKT.normals[i] );
+	}
+
+	SKT.RV[1]= SKT.V[0].copy();
+	SKT.RV[2]= SKT.V[0].copy();
+	SKT.RV[3]= SKT.V[1].copy();
+	SKT.RV[5]= SKT.V[2].copy();
+
+	//top leaf root -> left branch
+	SKT.RV[4] = maintain( SKT.V[1], SKT.V[0], SKT.bo[0] );
+	//flower root -> right branch
+	SKT.RV[6] = maintain( SKT.V[2], SKT.V[0], SKT.bo[1] );
+	//right leaf root -> right branch
+	SKT.RV[7] = maintain( SKT.V[2], SKT.V[0], SKT.bo[2] );
+
+
+	propagate(  SKT.V[0], SKT.V[1], SKT.l[0] );
+	propagate(  SKT.V[0], SKT.V[2], SKT.l[1] );
+	propagate( SKT.RV[3], SKT.V[3], SKT.l[2] );
+	propagate( SKT.RV[4], SKT.V[4], SKT.l[3] );
+	propagate( SKT.RV[5], SKT.V[5], SKT.l[4] );
+	propagate( SKT.RV[6], SKT.V[6], SKT.l[5] );
+	propagate( SKT.RV[7], SKT.V[7], SKT.l[6] );
+
+	for( var i = 0; i < 8; ++i ){
+		push();
+		if( SKT.RV[i] == null ){
+			translate( SKT.V[i].x, SKT.V[i].y );
+		}
+		else{
+			translate( SKT.RV[i].x, SKT.RV[i].y );
+			rotate( atan2( SKT.V[i].y - SKT.RV[i].y, SKT.V[i].x - SKT.RV[i].x ) + SKT.da[i] );
+		}
+
+		image( SKT.img[i], -SKT.O[i].x, -SKT.O[i].y, SKT.td[i].w, SKT.td[i].h );
+		pop();
+		//noFill();
+		//stroke(0);
+		//ellipse( SKT.V[i].x, SKT.V[i].y, SKT.contact_rad );
+		
+		//fill(0);
+		//ellipse(width/2, height/2,400 * SKT.Scl)
+	}
+}
+
+//// bkp draw
+
+function draw() {
+
+	SKT.draw();
+	//background(200,0,110)
+	if( INDEX > 0 ){
+		fill(255);
+		if( mouseX < VIEWPORT.x  ){
+			noStroke();
+			strokeWeight(3);
+		}
+		else noStroke();
+		triangle( VIEWPORT.x, tritop, VIEWPORT.x, tribot, 0, trimid );
+	}
+	if( INDEX < 21 ){
+		fill(255);
+		if( mouseX > VIEWPORT.r ){
+			noStroke();
+			strokeWeight(3);
+		}
+		else noStroke();
+		triangle( VIEWPORT.r, tritop, VIEWPORT.r, tribot, width, trimid );
+	}
+
+	push();
+	stroke(255);
+	strokeWeight(2);
+	for( var i = 0; i <= 21; i ++ ){
+
+		noFill();
+		if( mouseY > VIEWPORT.h &&
+				 mouseX > dootsx + ((i-0.5) * dootss) &&
+				 mouseX < dootsx + ((i+0.5) * dootss) ){
+
+			push();
+			textFont( DINcon, 20 );
+			fill(255);
+			noStroke();
+			textAlign( LEFT, BOTTOM );
+			text( titulos[i], dootsx, VIEWPORT.h );
+			pop();
+
+			fill(180);
+		}
+		if( INDEX == i ){
+			fill(255);
+		}
+		ellipse( dootsx + i *dootss, dootsy, dootsd );
+	}
+	pop();
+}
+
+mouse events bkp
+
+function mouseMoved() {
+
+	SKT.mouseMoved();
+}
+
+function mousePressed() {
+
+	if( coordinates_in_rct( mouseX, mouseY, VIEWPORT ) ){
+		SKT.mousePressed();
+	}
+	else{
+		cashmeoutsy = true;
+	}
+}
+
+function mouseDragged(){
+
+	SKT.mouseDragged();
+}
+
+function mouseReleased(){
+
+	if( coordinates_in_rct( mouseX, mouseY, VIEWPORT ) ){
+		SKT.mouseReleased();
+	}
+	else if( cashmeoutsy ){
+		if( mouseY > VIEWPORT.h ){
+			let pi = INDEX;
+			INDEX = round( (mouseX - dootsx) / dootss );
+			if( INDEX != pi && INDEX >= 0 && INDEX <= 21 ){
+				load_skt();
+			}
+		}
+		else{
+			if( mouseX < VIEWPORT.x ){
+				INDEX -= 1;
+			}
+			else INDEX += 1;
+
+			INDEX = constrain( INDEX, 0, 21 );
+			load_skt();
+		}
+	}
+	cashmeoutsy = false;
+}
+
+function keyReleased(){
+	if (keyCode === LEFT_ARROW) {
+		INDEX = constrain( INDEX - 1, 0, 21 );
+		load_skt();
+	}
+	else if (keyCode === RIGHT_ARROW) {
+		INDEX = constrain( INDEX + 1, 0, 21 );
+		load_skt();
+	}
+	else if( keyCode === 36 ){//home
+		INDEX = 0;
+		load_skt();
+	}
+	else if(keyCode === 35){//end
+		INDEX = 11;
+		load_skt();
+	}
+}
+
+function touchStarted() {
+
+	SKT.mousePressed();
+
+	if (INDEX > 0) {
+		var leftTriangleX = 5; // Margem de 5 pixels do início da tela
+		var leftTriangleY = height - 30;
+		var triangleSize = 15;
+		if (touches.length > 0) {
+		  var touchX = touches[0].x; // Obter a coordenada x do primeiro toque
+		  var touchY = touches[0].y; // Obter a coordenada y do primeiro toque
+		  if (touchX >= leftTriangleX && touchX <= leftTriangleX + triangleSize &&
+			  touchY >= leftTriangleY - triangleSize * 0.6 && touchY <= leftTriangleY + triangleSize * 0.6) {
+			INDEX--;
+			console.log(volta)
+			load_skt();
+			return; // Retorna para evitar que os dois triângulos sejam verificados ao mesmo tempo
+		  }
+		}
+	  }
+	
+	  // Verificar o toque no triângulo direito (avançar para a direita)
+	  if (INDEX < 21) {
+		var rightTriangleX = width - 5; // Margem de 5 pixels do fim da tela
+		var rightTriangleY = height - 30;
+		var triangleSize = 15;
+		if (touches.length > 0) {
+		  var touchX = touches[0].x; // Obter a coordenada x do primeiro toque
+		  var touchY = touches[0].y; // Obter a coordenada y do primeiro toque
+		  if (touchX <= rightTriangleX && touchX >= rightTriangleX - triangleSize &&
+			  touchY >= rightTriangleY - triangleSize * 0.6 && touchY <= rightTriangleY + triangleSize * 0.6) {
+			INDEX++;
+			console.log(avanca)
+			load_skt();
+		  }
+		}
+	  }
+	
+}
+
+  
+  function touchMoved() {
+	SKT.mouseDragged();
+  }
+
+function touchEnded() {
+	SKT.mouseReleased();
+/* 	if (touches.length > 0) {
+		SKT.mouseReleased();
+		 if (cashmeoutsy) {
+			if (touches[0].y > VIEWPORT.h) {
+				let pi = INDEX;
+				INDEX = round((touches[0].x - dootsx) / dootss);
+				if (INDEX !== pi && INDEX >= 0 && INDEX <= 21) {
+					load_skt();
+				}
+			} else {
+				if (touches[0].x < VIEWPORT.x) {
+					INDEX -= 1;
+				} else INDEX += 1;
+
+				INDEX = constrain(INDEX, 0, 21);
+				load_skt();
+			}
+		}
+		cashmeoutsy = false;
+		
+	} */
+}
+
 
 
 
